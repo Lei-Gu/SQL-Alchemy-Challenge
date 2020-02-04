611633e13eb28d6d10be57f7597f197052713f7c
Lei-Gu <leigu46@gmail.com>
2020-02-04 16:16:38 -0600

    updated codings

--- app.py
+++ app.py
@@ -38,15 +38,16 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-Passenger = Base.classes.passenger
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
@@ -63,49 +64,78 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    # Perform a query to retrieve the data and precipitation scores
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= '2016-08-22').order_by(Measurement.date).all()
+
+    precipitation_df = pd. DataFrame(precipitation, columns = ['date', 'prcp'])
+ 
+    # Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
+    precipitation__dict = dict(precipitation_df)
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
+    return jsonify(precipitation__dict)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
+@app.route("/api/v1.0/stations")
+def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
     # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    results = session.query(Stations.station, Stations.name).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    all_station = []
+    for station, name in results:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        all_station.append(station_dict)
 
-    return jsonify(all_passengers)
+    return jsonify(all_station)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # query for the dates and temperature observations from a year from the last data point.
+    results = session.query(Measurement.date,Measurement.tobs).\
+        filter(Measurement.date >= '2016-08-17').all()
+    
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_tobs = []
+    for date, tob in results:
+        tob_dict = {}
+        tob_dict["date"] = date
+        tob_dict["tobs"] = tobs
+        all_tobs.append(tob_dict)
+
+    return jsonify(all_tobs)
+
+    
+  
+
 
 
 if __name__ == '__main__':
